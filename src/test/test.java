3bc4676703bcb5b2de7be68a19d16535dcf12607
countkhalil <sirkhalil247@gmail.com>
2023-11-29 18:33:44 -0700

    first commit

---
+++ src/test/test.java
@@ -0,0 +1,36 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 200;
+ {if (x >300) {
+	 System.out.println("Outcome 1");
+ } else {
+	 System.out.println("Outcome 2"); }
+
+	 for (int i = 0; i <= 10; i += 1) {
+		 System.out.println( i);
+	 }
+	 int haha = 8;
+ do {
+	System.out.println(haha); 
+	haha ++;
+ }while (haha < 10) ;
+  
+ int mine = 20;
+ while (mine < 22) {
+	 System.out.println(mine);
+	 mine ++;
+ }
+	 
+ 
+ 
+	
+ 
+ }}}
+
+	
+	
+	
